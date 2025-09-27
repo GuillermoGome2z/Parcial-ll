443050dc457e45e2c65085f0197b8ded2aa70b26
GuillermoGome2z <gomezguille391@gmail.com>
2025-09-27 15:17:08 -0600

    creacion de la estructura

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  
+  return (
+    <div>
+      <h1>Bienvenido a la API de productos</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
